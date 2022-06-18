fdd939bf6ce37430b345750aafae3082ccdddde7
MrKrasnov <mr.krasnov.y@gmail.com>
2022-06-18 07:43:35 +0300

    0.3 finished contact component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.sass']
 })
 export class ContactComponent implements OnInit {
-
+  title: string = 'Small Web Studio'
+  content: string = 'Outsource web studio specializes in creating interesting web solutions. We can create perfect site for your product.'
+  logo: string = './assets/logo.png';
   constructor() { }
 
   ngOnInit(): void {
